ad75d78d463bf67d8a79c6c595b4919fbde0811d
Guillaume Faguet <guillaume.faguet@protonmail.com>
2023-12-13 15:40:53 +0100

    Day 12 solved

--- Day_12_Hot_Springs/part1_2.py
+++ Day_12_Hot_Springs/part1_2.py
@@ -1,7 +1,7 @@
-import itertools
+from functools import cache
 from pathlib import Path
 
-PUZZLE_FILENAME = "Day_12_Hot_Springs/example.txt"
+PUZZLE_FILENAME = "Day_12_Hot_Springs/puzzle_input.txt"
 
 
 def get_puzzle_lines() -> list[str]:
@@ -9,21 +9,38 @@ def get_puzzle_lines() -> list[str]:
         return f.read().splitlines()
 
 
+@cache
+def count(cfg: str, nums: tuple[int]) -> int:
+    if cfg == "":
+        return 1 if nums == () else 0
+    if nums == ():
+        return 0 if "#" in cfg else 1
+
+    result = 0
+    if cfg[0] in ".?":
+        result += count(cfg[1:], nums)
+    if cfg[0] in "#?":
+        if (
+            nums[0] <= len(cfg)
+            and "." not in cfg[: nums[0]]
+            and (nums[0] == len(cfg) or cfg[nums[0]] != "#")
+        ):
+            result += count(cfg[nums[0] + 1 :], nums[1:])
+        else:
+            result += 0
+    return result
+
+
 def main() -> None:
     grid = get_puzzle_lines()
-    r = (r.split() for r in grid)
-    for c, g in r:
-        total_hash = sum(map(int, g.split(",")))
-        hash_in_string = c.count("#")
-        question_in_string = c.count("?")
-        hash_to_place = total_hash - hash_in_string
-        combs = itertools.permutations(
-            (question_in_string - hash_to_place) * "." + hash_to_place * "#",
-            question_in_string,
-        )
-        print(list(combs))
-        input()
-    # print(list(itertools.permutations("###..", 5)))
+    total = 0
+    for line in grid:
+        cfg, nums = line.split()
+        nums = tuple(map(int, nums.split(",")))
+        cfg = "?".join([cfg] * 5)
+        nums *= 5
+        total += count(cfg, nums)
+    print(total)
 
 
 if __name__ == "__main__":
