99acae84a8206eabf1b49c3030d4fa88025d7cc1
Guillaume Faguet <guillaume.faguet@protonmail.com>
2024-12-02 17:16:01 +0100

    day2 solved

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -1,18 +1,33 @@
+from collections import Counter
+
 import aoc_lube
 
 RAW = aoc_lube.fetch(year=2024, day=1)
-print(RAW)
+# print(RAW)
 
-def parse_raw():
-    ...
+def parse_raw():  # noqa: ANN201, D103
+    first_col=[int(row.split()[0]) for row in RAW.split("\n")]
+    second_col=[int(row.split()[1]) for row in RAW.split("\n")]
+    first_col.sort()
+    second_col.sort()
+    return first_col,second_col
+
 
 DATA = parse_raw()
 
-def part_one():
-    ...
+def part_one():  # noqa: ANN201, D103
+    differences=[abs(a-b) for a,b in zip(DATA[0],DATA[1])]
+    return sum(differences)
 
-def part_two():
-    ...
 
-aoc_lube.submit(year=2024, day=1, part=1, solution=part_one)
-aoc_lube.submit(year=2024, day=1, part=2, solution=part_two)
+
+def part_two():  # noqa: ANN201, D103
+    second_col_count=Counter(DATA[1])
+    sim_score=0
+    for elem in DATA[0]:
+        sim_score+=elem*second_col_count[elem]
+    return sim_score
+
+
+# aoc_lube.submit(year=2024, day=1, part=1, solution=part_one)
+# aoc_lube.submit(year=2024, day=1, part=2, solution=part_two)
--- 2024/day_02.py
+++ 2024/day_02.py
@@ -1,18 +1,33 @@
 import aoc_lube
 
 RAW = aoc_lube.fetch(year=2024, day=2)
-print(RAW)
 
 def parse_raw():
-    ...
+    return [list(map(int,line.split())) for line in RAW.splitlines()]
 
 DATA = parse_raw()
+print(DATA)
+
+def safe(row):
+    return all(a<b and 1<=abs(a-b)<=3 for a,b in zip(row,row[1:])) or all(a>b and 1<=abs(a-b)<=3 for a,b in zip(row,row[1:]))
+
+def all_decreasing(row):
+    return all(a>b and 1<=abs(a-b)<=3 for a,b in zip(row,row[1:]))
 
 def part_one():
-    ...
+    safe_reports=0
+    for row in DATA:
+        if safe(row):
+            safe_reports+=1
+    return safe_reports
 
 def part_two():
-    ...
+    safe_reports=0
+    for row in DATA:
+        if any(safe(row[:i]+row[i+1:]) for i in range(len(row))):
+                safe_reports+=1
+    return safe_reports
+
 
 aoc_lube.submit(year=2024, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=2, part=2, solution=part_two)
