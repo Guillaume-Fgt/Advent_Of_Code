192dca458c580b33e05339db8f77f0de9f27cb34
Guillaume Faguet <guillaume.faguet@protonmail.com>
2024-12-08 11:16:32 +0100

    day 6 & 7 solved

--- 2024/day_06.py
+++ 2024/day_06.py
@@ -1,18 +1,42 @@
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2024, day=6)
-print(RAW)
+GRID = aoc_lube.fetch(year=2024, day=6).splitlines()
+START = next(
+    (y, x) for y, line in enumerate(GRID) for x, char in enumerate(line) if char == "^"
+)
+H = len(GRID)
+W = len(GRID[0])
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def patrol(oy=-1, ox=-1):
+    seen = set()
+    y, x = START
+    dy, dx = -1, 0
+    while True:
+        if not (0 <= y < H and 0 <= x < W):
+            return len(seen) if oy == -1 else False
+        if GRID[y][x] == "#" or (y == oy and x == ox):
+            y -= dy
+            x -= dx
+            dy, dx = dx, -dy
+        elif (y, x, dy, dx) in seen:
+            return True
+        else:
+            if oy == -1:
+                seen.add((y, x))
+            else:
+                seen.add((y, x, dy, dx))
+            y += dy
+            x += dx
+
 
 def part_one():
-    ...
+    return patrol()
+
 
 def part_two():
-    ...
+    return sum(patrol(y, x) for y in range(H) for x in range(W) if GRID[y][x] == ".")
+
 
 aoc_lube.submit(year=2024, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=6, part=2, solution=part_two)
--- 2024/day_07.py
+++ 2024/day_07.py
@@ -1,18 +1,47 @@
+import itertools
+
 import aoc_lube
 
 RAW = aoc_lube.fetch(year=2024, day=7)
-print(RAW)
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def can_obtain(target, array):
+    if len(array) == 1:
+        return target == array[0]
+    if target % array[-1] == 0 and can_obtain(target // array[-1], array[:-1]):
+        return True
+    if target > array[-1] and can_obtain(target - array[-1], array[:-1]):
+        return True
+    if (
+        len(str(target)) > len(str(array[-1]))
+        and str(target).endswith(str(array[-1]))
+        and can_obtain(int(str(target)[: -len(str(array[-1]))]), array[:-1])
+    ):
+        return True
+    return False
 
 def part_one():
-    ...
+    total = 0
+    for line in RAW.splitlines():
+        left, right = line.split(": ")
+        target = int(left)
+        array = [int(x) for x in right.split()]
+
+        if can_obtain(target, array):
+            total += target
+    return total
 
 def part_two():
-    ...
+    total = 0
+    for line in RAW.splitlines():
+        left, right = line.split(": ")
+        target = int(left)
+        array = [int(x) for x in right.split()]
 
-aoc_lube.submit(year=2024, day=7, part=1, solution=part_one)
+        if can_obtain(target, array):
+            total += target
+    return total
+
+print(part_one())
+# aoc_lube.submit(year=2024, day=7, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=7, part=2, solution=part_two)
